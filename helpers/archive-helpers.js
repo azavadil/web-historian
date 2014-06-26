bbd4bcd2661e977dbd312b72f9304789b25c0107
Hack Reactor Students <anthony.zavadil@gmail.com>
2014-06-25 20:12:38 -0700

    finished serving up cached pages, starting with webscraping

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -12,7 +12,8 @@ var _ = require('underscore');
 exports.paths = filePaths = {
   'siteAssets' : path.join(__dirname, '../web/public'),
   'archivedSites' : path.join(__dirname, '../archives/sites'),
-  'list' : path.join(__dirname, '../archives/sites.txt')
+  'list' : path.join(__dirname, '../archives/sites.txt'),
+  'tmp' : path.join(__dirname, '../archives/tmp.txt')
 };
 
 // Used for stubbing paths for jasmine tests, do not modify
@@ -25,24 +26,42 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfUrls = readListOfUrls = function(url, callback){
   var list = fs.readFile(filePaths.list, function(err, data){
     data = data.toString().split(',');
+    data = data.length > 1 ? data.slice(0,data.length-1) : data;
     var urls = {};
+  console.log('data', data);
     for(var i = 0; i < data.length; i++){
-      urls[data[i]] = '_';
+      urls[data[i]] = true;
     }
-    return urls;
+    if(urls[url]){
+      callback(true);
+    } else {
+      callback(false);
+      exports.addUrlToList(url);
+    }
   });
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(url, callback){
+  readListOfUrls(url, callback);
+
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(url){
+  fs.appendFile(filePaths.tmp, url.trim()+',', function(err){
+    if(err) throw 'addUrlToList failed';
+  });
 };
 
+// used by cronApp
 exports.isURLArchived = function(){
+};
+
+// used by
+exports.getURLHtml = function(){
+
 };
 
 exports.downloadUrls = function(){
